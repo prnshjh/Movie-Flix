56c3ed8047c1dccd9aea0c59292d291e55d6f029
jhapriyanshu19 <prnshjh@gmail.com>
2025-02-07 17:14:59 +0530

    beggining

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>
+            Copyright 2025 My Website 
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/Components/Main.jsx
@@ -0,0 +1,16 @@
+
+
+const Main = () => {
+  return (
+    <main>
+      <h1>
+        Main content
+      </h1>
+      <p>
+        This is the main content
+      </p>
+    </main>
+  )
+}
+
+export default Main;
