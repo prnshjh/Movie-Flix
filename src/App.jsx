b3c559e03281c2bbffbd8bb7a42ba846c759b5e1
jhapriyanshu19 <prnshjh@gmail.com>
2025-02-09 00:14:12 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import UserList from './Components/UserList'
 
 const App = () => {
-  return (
-    <div><UserList/></div>
-  )
+
+  return <User
+  img="https://picsum.photos/id/237/200/300"
+  name="Priyanshu Jha"
+  age={22}
+  isMarraid={false}
+  hobbies={["Coding", "Reading", "Singing"]}
+  />
+};
+
+const User =(props)=>{
+return(
+  <section>
+    <img src="https://picsum.photos/id/237/200/300" alt="picture" />
+    <h1>Name: {props.name}</h1>
+    <h1>Age: {props.age}</h1>
+    <h1>isMarraid: {props.isMarraid}</h1>
+    <h1>Hobbies: {props.hobbies}</h1>
+  </section>
+)
 }
 
 export default App
