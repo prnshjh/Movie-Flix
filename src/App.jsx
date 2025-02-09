0a13b8d711096ae1fdd546b86fcf7cbd74093d04
jhapriyanshu19 <prnshjh@gmail.com>
2025-02-09 10:55:48 +0530

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import Person from './Person'
-import Product from './Product'
+const ValidPassword=()=><h1>Valid Password</h1>;
+const InvalidPassword=()=><h2>Invalid passowrd</h2>;
+
+const Password=({isValid})=>{
+  return isValid? <ValidPassword/>:<InvalidPassword/>
+};
 
 const App = () => {
-  return (<div>
-  <Person name="Priyanshu" age={21} />
-  <Product name="Iphone" price={799}/>
-  </div>
+  return (
+    <div>
+      <Password isValid={false}/>
+    </div>
   )
 }
 
