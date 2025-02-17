2ca6edfc6736b140c38d27b59a0c7397d43024a4
jhapriyanshu19 <prnshjh@gmail.com>
2025-02-18 01:47:51 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
-const Move=()=>{
-  const mouseMove=()=>{
-  alert("Mouse Movement Found");
-  console.log("Mouse Movement Detected");
-  }
-  return(
-    <p onMouseMove={mouseMove}></p>
-  )
-}
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import React, { useState } from "react";
 
-export default App
+const App = () => {
+  const [movies, setMovies] = useState([
+    { id: 1, title: "Spiderman", Ratings: 3 },
+    { id: 2, title: "Superman", Ratings: 5 },
+    { id: 3, title: "Batman", Ratings: 7 },
+    { id: 4, title: "Ironman", Ratings: 8.4 },
+  ]);
+
+  const handleClick = () => {
+    setMovies(
+      movies.map((m) =>
+        m.id === 1 ? { ...m, title: "John Wick 5", Ratings: 6 } : m
+      )
+    );
+  };
+
+  return (
+    <div>
+      <ul>
+        {movies.map((m) => (
+          <li key={m.id}>{m.title}</li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Change Title</button>
+    </div>
+  );
+};
+
+export default App;
